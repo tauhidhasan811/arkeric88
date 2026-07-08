134091a9a7176cd00590f2042c69355f7633cb4d
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-07-08 10:26:28 +0600

    add google api key

--- src/config/config.py
+++ src/config/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     openai_api_key: str
+    google_api_key :str
 
     model_config = SettingsConfigDict(
         env_file=".env",
